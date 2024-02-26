283481be3e05439e47a757dfcd08a51b9256e587
Chrisio (Weixi Guan) Gwaan <chris322322@gmail.com>
2024-02-26 16:02:10 +0800

    Update copyright
    Co-authored-by: Eva Evgenidis <eva-e@users.noreply.github.com>

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Chrisio
+      <time>{YEAR}</time> © Chrisio & Evanthia Portfolio. All rights reserved.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
