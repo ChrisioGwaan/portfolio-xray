18b36661ab206c77f677a85ea5f35ad9862d4c09
chrisiogwaan <chris322322@gmail.com>
2025-01-08 13:31:33 +1100

    Folder changed and fixed duplicated project pictures

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Header } from '@/components/Header'
-export { Footer } from '@/components/Footer'
-export { Mailchimp } from '@/components/Mailchimp'
-export { ProjectCard } from '@/components/ProjectCard'
-export { HeadingLink } from '@/components/HeadingLink'
-export { RouteGuard } from '@/components/RouteGuard'
